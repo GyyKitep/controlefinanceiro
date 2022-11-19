8cc9a20d3b120603f9964e1ef57ed6a80a32fe2b
Geovanny Gonsales <geovanny.gonsales.dev@gmail.com>
2022-11-19 14:07:06 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/controlefinanceiro/ControlefinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlefinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlefinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlefinanceiroApplication.class, args);
+	}
+
+}
